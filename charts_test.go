ad4293bc51cfc071c10e5c64fbff9897ddea9d4a
Aaron Bycoffe <bycoffe@gmail.com>
2014-11-18 14:53:37 -0500

    Remove print statements from test

--- charts_test.go
+++ charts_test.go
@@ -1,7 +1,6 @@
 package pollster
 
 import "testing"
-import "fmt"
 
 func TestCharts(t *testing.T) {
 	params := map[string]string{
@@ -10,17 +9,5 @@ func TestCharts(t *testing.T) {
 	charts := Charts(params)
   if len(charts) == 0 {
     t.Errorf("Found 0 charts; want > 0")
-  }
-  xparams := map[string]string{
-    "state": "CO",
-    "topic": "2014-senate",
-  }
-  polls := Polls(xparams)
-  for _, poll := range polls {
-    fmt.Printf("\n%s: %s\n", poll.Pollster, poll.Method)
-    for _, question := range poll.Questions {
-      subpop := question.Subpopulations[0]
-      fmt.Printf("%s: %s (%d)\n", question.Name, subpop.Name, subpop.Observations)
-    }
   }
 }
